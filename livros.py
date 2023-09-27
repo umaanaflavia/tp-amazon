05817b9ecce73c46eab22258bcc04b5ca68086f1
umaanaflavia <64756691+umaanaflavia@users.noreply.github.com>
2023-09-27 00:05:26 -0300

    Add files via upload

---
+++ livros.py
@@ -0,0 +1,92 @@
+import csv
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import re
+
+link_file = "link-livros.csv"
+output_file = "livros.csv"
+
+# Configure Chrome options for headless mode and logging level
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument('--log-level=3')
+
+driver = webdriver.Chrome(service=Service('chromedriver.exe'), options=chrome_options)
+
+# Define regular expressions for filtering lines
+regex_patterns = [
+    r'.*páginas$',
+    r'^ISBN-10 :',
+    r'^ISBN-13 :',
+    r'^Ranking dos mais vendidos:',
+    r'^Avaliações dos clientes:',
+    r'.* avaliações de clientes'
+]
+
+with open(link_file, 'r') as file:
+    csv_reader = csv.reader(file)
+    
+    for row in csv_reader:
+        url = row[0]
+        while True:
+            driver.get(url)
+            
+            # Check if the "Request was throttled" message is present
+            if "Request was throttled. Please wait a moment and refresh the page" in driver.page_source:
+                print("Throttling detected. Refreshing the page...")
+                time.sleep(2)  # Wait for a moment before refreshing
+                continue  # Reload the page
+            else:
+                break  # Exit the loop if the message is not present
+            
+        # Check if the page contains the text "Frequentemente comprados juntos"
+        if "Frequentemente comprados juntos" in driver.page_source:
+            # Click the "Leia mais" element if it exists
+            try:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                leia_mais_element = driver.find_element(By.PARTIAL_LINK_TEXT, "Leia mais")
+                leia_mais_element.click()
+            except:
+                pass
+            
+            # Find the element with the ID "bookDescription_feature_div"
+            book_description = driver.find_element(By.ID, "bookDescription_feature_div")
+            
+            # Find the element with the class "a-expander-content" within "bookDescription_feature_div"
+            expander_content = book_description.find_element(By.CLASS_NAME, "a-expander-content")
+            
+            # Extract the content of the element and remove newline characters and quotation marks
+            book_description_content = expander_content.text.replace('\n', '').replace('"', '')
+            
+            # Find the element with the class "_p13n-desktop-sims-fbt_fbt-desktop_new-detail-faceout-box___WyNy"
+            faceout_box = driver.find_element(By.CLASS_NAME, "_p13n-desktop-sims-fbt_fbt-desktop_new-thumbnail-box__36bD3")
+            
+            # Find all elements with the class "a-link-normal" within "_p13n-desktop-sims-fbt_fbt-desktop_new-detail-faceout-box___WyNy"
+            link_elements = faceout_box.find_elements(By.CLASS_NAME, "_p13n-desktop-sims-fbt_fbt-desktop_new-detail-faceout-box___WyNy")
+            
+            link_list = []
+            link_titles = []
+            for i in range(1, len(link_elements)):
+                link_list.append(link_elements[i].find_element(By.TAG_NAME, "a").get_attribute('href'))
+                link_titles.append(link_elements[i].find_element(By.CLASS_NAME, "a-size-base").text)
+            
+            # Find the element with the ID "detailBullets_feature_div"
+            detail_bullets = driver.find_element(By.ID, "detailBullets_feature_div")
+            
+            # Extract the content of the element and split it into lines
+            detail_bullets_content = detail_bullets.text.split('\n')
+            
+            # Filter lines using regular expressions
+            filtered_lines = [line for line in detail_bullets_content if any(re.match(pattern, line) for pattern in regex_patterns)]
+            
+            # Write the filtered content to the output CSV file
+            with open(output_file, 'a', newline='', encoding='utf-8') as csv_file:
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerow([url, book_description_content] + filtered_lines +  [link_list] + [link_titles])
+        else:
+            print("Text 'Frequentemente comprados juntos' not found on the page.")
+
+driver.quit()
---
+++ mais-vendidos.py
@@ -0,0 +1,62 @@
+import csv
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=Service('chromedriver.exe'))
+url = "https://www.amazon.com.br/gp/bestsellers/books/7873971011/ref=zg_bs_pg_1_books?ie=UTF8&pg=1"
+
+while True:
+    driver.get(url)
+    
+    # Check if the "Request was throttled" message is present
+    if "Request was throttled. Please wait a moment and refresh the page" in driver.page_source:
+        print("Throttling detected. Refreshing the page...")
+        time.sleep(2)  # Wait for a moment before refreshing
+        continue  # Reload the page
+    else:
+        break  # Exit the loop if the message is not present
+
+# Create and open a CSV file for writing information
+with open('mais-vendidos.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    details_csv_writer = csv.writer(csvfile)
+    
+    # Create and open a CSV file for writing href values
+    with open('link-livros.csv', 'w', newline='', encoding='utf-8') as href_csvfile:
+        href_csv_writer = csv.writer(href_csvfile)
+    
+        while True:
+            # Scroll to the bottom of the page using JavaScript
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)  # Sleep for 2 seconds after scrolling
+            
+            # Find all elements with ids matching the pattern p13n-asin-index-*
+            elements = driver.find_elements(By.CSS_SELECTOR, '[id^="p13n-asin-index-"]')
+            
+            # Loop through the elements
+            for element in elements:
+                # Split element.text into lines
+                lines = element.text.split('\n')
+                
+                # Find the a-link-normal element inside the div
+                link_element = element.find_element(By.CLASS_NAME, 'a-link-normal')
+                
+                # Get the href attribute value
+                href = link_element.get_attribute('href')
+                
+                # Write the href to the href CSV file
+                href_csv_writer.writerow([href])
+                
+                # Write the details to the informations CSV file
+                details_csv_writer.writerow(lines)
+            
+            # Check if there is a "Próxima página" button
+            next_page_button = driver.find_element(By.PARTIAL_LINK_TEXT, "Próxima página")
+            
+            if next_page_button.is_enabled():
+                next_page_button.click()
+            else:
+                break  # Exit the loop if the "Próxima página" button is not available
+
+driver.quit()
