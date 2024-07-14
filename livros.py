a8f6813a2f341174804b2f12387fe295443a5f24
Lucas Mesquita <75052611+MesquitaALucas@users.noreply.github.com>
2024-07-14 00:26:57 -0300

    base e codigo de coleta da base

--- livros.py
+++ livros.py
@@ -46,7 +46,9 @@ chrome_options = Options()
 # chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument('--log-level=3')
-driver = webdriver.Chrome(service=Service('chromedriver.exe'), options=chrome_options)
+chromedriver_path = r'C:\Users\lucas\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe'
+
+driver = webdriver.Chrome(service=Service(chromedriver_path), options=chrome_options)
 
 # Define regular expressions for filtering lines
 regex_patterns = [
@@ -99,76 +101,74 @@ with open(link_file, 'r') as file:
             except:
                 book_category = ""
 
-            if "Política" in " ".join(book_category):
-                try:
-                    book_title = driver.find_element(By.ID, "productTitle").text
-                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                    time.sleep(1)
-                    # Click the "Leia mais" element if it exists
-                    leia_mais_element = driver.find_element(By.PARTIAL_LINK_TEXT, "Leia mais")
-                    leia_mais_element.click()
-                except:
-                    book_title = ""
+            
+            try:
+                book_title = driver.find_element(By.ID, "productTitle").text
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(1)
+                # Click the "Leia mais" element if it exists
+                leia_mais_element = driver.find_element(By.PARTIAL_LINK_TEXT, "Leia mais")
+                leia_mais_element.click()
+            except:
+                book_title = ""
                 
-                if (book_title != "") and (not is_book_in_file(book_title, output_file)):
-                    # Retry finding the elements in case of failure
-                    fail = 1
-                    while fail:
+            if (book_title != "") and (not is_book_in_file(book_title, output_file)):
+                # Retry finding the elements in case of failure
+                fail = 1
+                while fail:
+                    try:
+                        book_description = driver.find_element(By.ID, "bookDescription_feature_div")
+                        expander_content = book_description.find_element(By.CLASS_NAME, "a-expander-content")
                         try:
-                            book_description = driver.find_element(By.ID, "bookDescription_feature_div")
-                            expander_content = book_description.find_element(By.CLASS_NAME, "a-expander-content")
-                            try:
-                                book_description_content = expander_content.text.replace('\n', '').replace('"', '')
-                            except:
-                                book_description_content = ""
-                                pass
+                            book_description_content = expander_content.text.replace('\n', '').replace('"', '')
+                        except:
+                            book_description_content = ""
+                            pass
                             
-                            faceout_box = driver.find_element(By.CLASS_NAME, "p13n-sc-shoveler")
-                            link_elements = faceout_box.find_elements(By.CLASS_NAME, "p13n-sc-uncoverable-faceout")
+                        faceout_box = driver.find_element(By.CLASS_NAME, "p13n-sc-shoveler")
+                        link_elements = faceout_box.find_elements(By.CLASS_NAME, "p13n-sc-uncoverable-faceout")
                             
-                            fail = 0
-                            break  # Elements found successfully, exit retry loop
+                        fail = 0
+                        break  # Elements found successfully, exit retry loop
                         
-                        except Exception as e:
-                            print(f"Failed to find elements: {str(e)}")
-                            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                            time.sleep(2)  # Wait for a moment before retrying
+                    except Exception as e:
+                        print(f"Failed to find elements: {str(e)}")
+                        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                        time.sleep(2)  # Wait for a moment before retrying
 
-                    print(index+2, " - ", book_title)
-                    link_list = []
-                    link_titles = []
-                    for i in range(0, len(link_elements)):
-                        link = link_elements[i].find_element(By.TAG_NAME, "a").get_attribute('href')
-                        cleaned_link = remove_ref_from_link(link)  # Remove everything after "ref="
-                        link_titles.append(link_elements[i].find_element(By.CLASS_NAME, "p13n-sc-truncate-desktop-type2").text)
+                print(index+2, " - ", book_title)
+                link_list = []
+                link_titles = []
+                for i in range(0, len(link_elements)):
+                    link = link_elements[i].find_element(By.TAG_NAME, "a").get_attribute('href')
+                    cleaned_link = remove_ref_from_link(link)  # Remove everything after "ref="
+                    link_titles.append(link_elements[i].find_element(By.CLASS_NAME, "p13n-sc-truncate-desktop-type2").text)
+                    
+                    if not is_link_in_file(cleaned_link, link_file):  # Check if the link is not already in the file
+                        link_list.append(cleaned_link)
+                        # Append the cleaned link to the end of the link_file
+                        with open(link_file, 'a', newline='', encoding='utf-8') as link_file_append:
+                            link_file_writer = csv.writer(link_file_append)
+                            link_file_writer.writerow([cleaned_link])
                         
-                        if not is_link_in_file(cleaned_link, link_file):  # Check if the link is not already in the file
-                            link_list.append(cleaned_link)
-                            # Append the cleaned link to the end of the link_file
-                            with open(link_file, 'a', newline='', encoding='utf-8') as link_file_append:
-                                link_file_writer = csv.writer(link_file_append)
-                                link_file_writer.writerow([cleaned_link])
-                        
-                    # Find the element with the ID "detailBullets_feature_div"
-                    detail_bullets = driver.find_element(By.ID, "detailBullets_feature_div")
+                # Find the element with the ID "detailBullets_feature_div"
+                detail_bullets = driver.find_element(By.ID, "detailBullets_feature_div")
                     
-                    # Extract the content of the element and split it into lines
-                    detail_bullets_content = detail_bullets.text.split('\n')
+                # Extract the content of the element and split it into lines
+                detail_bullets_content = detail_bullets.text.split('\n')
+                
+                # Filter lines using regular expressions
+                filtered_lines = [line for line in detail_bullets_content if any(re.match(pattern, line) for pattern in regex_patterns)]
                     
-                    # Filter lines using regular expressions
-                    filtered_lines = [line for line in detail_bullets_content if any(re.match(pattern, line) for pattern in regex_patterns)]
-                    
-                    # Write the filtered content to the output CSV file
-                    with open(output_file, 'a', newline='', encoding='utf-8') as csv_file:
-                        csv_writer = csv.writer(csv_file)
-                        row = [url, book_title, book_category, book_description_content, *filtered_lines, link_titles]
-                        handle_missing_book_data(row)
-                        csv_writer.writerow(row)
-                else:
-                    print("Book title already on file.")
-                    continue
+                # Write the filtered content to the output CSV file
+                with open(output_file, 'a', newline='', encoding='utf-8') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    row = [url, book_title, book_category, book_description_content, *filtered_lines, link_titles]
+                    handle_missing_book_data(row)
+                    csv_writer.writerow(row)
             else:
-                print("'Política' does not appear in the book categories.")
+                print("Book title already on file.")
+                continue
         else:
             print("Text 'Clientes que compraram este item também compraram' not found on the page.")
 
--- mais-vendidos.py
+++ mais-vendidos.py
@@ -7,83 +7,89 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 
-# Initialize the Chrome driver with options
+# Caminho para o ChromeDriver baixado
+chromedriver_path = r'C:\Users\lucas\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe'
+
+# Inicializa as opções do Chrome
 chrome_options = Options()
 # chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument('--log-level=3')
-driver = webdriver.Chrome(service=Service('chromedriver.exe'), options=chrome_options)
 
-url = "https://www.amazon.com.br/gp/bestsellers/books/7873971011/ref=zg_bs_pg_1_books?ie=UTF8&pg=1"
+# Inicializa o driver do Chrome
+driver = webdriver.Chrome(service=Service(chromedriver_path), options=chrome_options)
+
+url = "https://www.amazon.com.br/gp/bestsellers/books/ref=zg_bs_pg_1_books?ie=UTF8&pg=1"
 data = []
 link_list = []
 
 while True:
     driver.get(url)
     
-    # Check if the "Request was throttled" message is present
+    # Verifica se há mensagem de "Request was throttled"
     if "Request was throttled. Please wait a moment and refresh the page" in driver.page_source:
         print("Throttling detected. Refreshing the page...")
-        time.sleep(2)  # Wait for a moment before refreshing
-        continue  # Reload the page
+        time.sleep(2)  # Espera antes de atualizar a página
+        continue  # Recarrega a página
     else:
-        break  # Exit the loop if the message is not present
+        break  # Sai do loop se a mensagem não estiver presente
 
-# Create and open a CSV file for writing information
+# Cria e abre um arquivo CSV para escrever as informações
 with open('mais-vendidos.csv', 'w', newline='', encoding='utf-8') as csvfile:
     details_csv_writer = csv.writer(csvfile)
     
-# Create and open a CSV file for writing href values
+# Cria e abre um arquivo CSV para escrever os valores de href
     while True:
-        # Scroll to the bottom of the page using JavaScript
+        # Faz scroll até o final da página usando JavaScript
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)  # Sleep for 2 seconds after scrolling
+        time.sleep(2)  # Espera 2 segundos após o scroll
         
-        # Find all elements with ids matching the pattern p13n-asin-index-*
+        # Encontra todos os elementos com ids correspondendo ao padrão p13n-asin-index-*
         elements = driver.find_elements(By.CSS_SELECTOR, '[id^="p13n-asin-index-"]')
         
-        # Loop through the elements
+        # Itera sobre os elementos
         for element in elements:
-            # Split element.text into lines
+            # Divide o texto do elemento em linhas
             lines = element.text.split('\n')
-            # Set the field "num. of reviews" as an empty string if there are no reviews
+            # Define o campo "num. of reviews" como uma string vazia se não houver avaliações
             len_without_reviews = 5
             reviews_position = 3
             if len(lines) == len_without_reviews:
                 lines.insert(reviews_position, '')
             
-            # Find the a-link-normal element inside the div
+            # Encontra o elemento a-link-normal dentro do div
             link_element = element.find_element(By.CLASS_NAME, 'a-link-normal')
             
-            # Get the href attribute value
+            # Obtém o valor do atributo href
             href = link_element.get_attribute('href')
             
-            # Remove everything after "ref=" in href
+            # Remove tudo após "ref=" em href
             link = href.split('ref=')[0]
             
-            # 
+            # Adiciona os dados à lista data
             data.append(lines)
             
-            # Write the details to the informations CSV file
+            # Adiciona o link à lista link_list
             link_list.append(link)
         
-        # Check if there is a "Próxima página" button
+        # Verifica se há um botão "Próxima página"
         try:
             next_page_button = driver.find_element(By.PARTIAL_LINK_TEXT, "Próxima página")
             if next_page_button.is_enabled():
                 next_page_button.click()
             else:
-                break  # Exit the loop if the "Próxima página" button is not available
-        except NoSuchElementException: # NoSuchElementException is lauched if the element is not foud i.e. if  there's no next page
-            break
+                break  # Sai do loop se o botão "Próxima página" não estiver disponível
+        except NoSuchElementException:
+            break  # Sai do loop se não houver próxima página
 
-# Create Pandas DataFrames from the collected data
+# Cria DataFrames do Pandas com os dados coletados
 df = pd.DataFrame(data)
 df.columns = ['position', 'name', 'publisher/author', 'num. of reviews', 'cover type', 'price']
 href_df = pd.DataFrame(link_list, columns=['url'])
 
-# Write the DataFrames to CSV files
+# Escreve os DataFrames em arquivos CSV
 df.to_csv('mais-vendidos.csv', index=False, encoding='utf-8')
 href_df.to_csv('link-livros.csv', index=False, encoding='utf-8')
 
+# Fecha o navegador
 driver.quit()
